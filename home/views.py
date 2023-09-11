42ec5381c92438d58eb600880526c24743aab958
Terdmannsdorffer <terdmannsdorffer@miuandes.cl>
2023-09-11 18:40:59 -0300

    check_answer method

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+import json
 
 from .models import *
 import random
@@ -32,3 +33,33 @@ def get_quiz(request):
     except Exception as e:
         print(e)
         return HttpResponse("Error")
+    
+
+
+def check_answer(request):
+    if request.method == 'POST':
+        try:
+            data = request.POST.get('data')  
+
+            
+            data = json.loads(data)
+            question_id = data.get('question')
+            selected_answer_id = data.get('answer')
+
+            
+            question = Question.objects.get(pk=question_id)
+            correct_answer = Answer.objects.get(question=question, is_correct=True)
+
+            
+            is_correct = selected_answer_id == correct_answer.pk
+
+            response_data = {
+                'is_correct': is_correct,
+            }
+
+            return JsonResponse(response_data)
+        except Exception as e:
+            print(e)
+            return JsonResponse({'is_correct': False, 'error': str(e)})
+
+    return JsonResponse({'is_correct': False, 'error': 'Invalid request method'})
